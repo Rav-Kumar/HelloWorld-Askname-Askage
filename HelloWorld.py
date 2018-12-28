186b8a95090102f4ee8efce19389e78a3a36cb2a
Ravi Kumar <kumar.permjobs@outlook.com>
2018-12-28 17:17:12 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,19 @@
+#Greet and ask for name, age
+
+print ('Hello World!')
+print ('what is your name?')
+
+myname = input ()
+
+print ('Good to meet you, '+ myname)
+    
+print ('your name has ' + str (len (myname)) + ' characters')
+print ('what is your age')
+
+myage = input ()
+
+print ('you will be ' + str (int (myage) + 1) + ' next year')
+print ('have a good day!!')
+
+       
+       
